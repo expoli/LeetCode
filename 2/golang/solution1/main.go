01adf9b1062fe668d31dd3b29b16e09999839714
expoli <31023767+expoli@users.noreply.github.com>
2023-01-28 19:52:18 +0800

    添加第二题的注释

--- 2/golang/solution1/main.go
+++ 2/golang/solution1/main.go
@@ -22,6 +22,10 @@ func main() {
 	printList(sum)
 }
 
+// ListNode
+/*
+单向链表定义
+*/
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -61,14 +65,14 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		l1 = l1.Next
 		l2 = l2.Next
 	}
-
+	// 进行后续数据结点的处理
 	var lastList *ListNode
 	if l1 != nil {
 		lastList = l1
 	} else {
 		lastList = l2
 	}
-
+	// 求和计算
 	for lastList != nil {
 		sum := lastList.Val
 		if flag == 1 {
@@ -83,23 +87,29 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		}
 		lastList = lastList.Next
 	}
+	// 最高位有进位的情况
 	if flag == 1 {
 		temp = append(temp, 1)
 	}
-
+	// 根据切片创建链表，这个是否能够优化掉呢？（比如一边计算一边构建链表）
 	number := createANumber(temp)
 	return number
 }
 
+/*
+根据输入的整数切片进行数字链表的构建，头结点进行特殊处理
+*/
 func createANumber(nums []int) *ListNode {
 	var header ListNode
 	var prev *ListNode
 	for i, num := range nums {
+		// 初始化头结点
 		if i == 0 {
 			header.Val = num
 			header.Next = nil
 			prev = &header
 		} else {
+			// 向链表中添加其它的数据结点
 			var temp = ListNode{
 				Val:  num,
 				Next: nil,
