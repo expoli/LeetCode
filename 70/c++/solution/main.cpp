4c6eb380ca35e1838cbde472ed906275dc21f3e6
expoli <31023767+expoli@users.noreply.github.com>
2023-03-19 22:36:12 +0800

    递推算法

--- 70/c++/solution/main.cpp
+++ 70/c++/solution/main.cpp
@@ -22,7 +22,21 @@ public:
         }
         return data[n];
     }
+
+    int climbStairs2(int n) {
+        int dp[n+1] = {0};
+        if(n == 1|| n == 2){
+            return n;
+        }
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3;i <= n; ++i){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
 };
+
 int main()
 {
     Solution solution;
